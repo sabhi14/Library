103550fb19e3cebd9aff190fd5afbd44a696bccb
sabhi14 <47172839+sabhi14@users.noreply.github.com>
2019-11-01 01:08:27 +0530

    Update truyum-data.sql

--- truyum-data.sql
+++ truyum-data.sql
@@ -1,9 +1,9 @@
 use truyum;
-insert into menu_item values('Sandwich',99.00,'Yes','2017-03-15','Main Course','Yes'),
-	('Burger',129.00,'Yes','2017-12-27','Main Course','No'),
-	('Pizza', 149.00, 'Yes', '2018-08-21', 'Main Course', 'No'),
-	('French Fries', 57.00, 'No', '2017-07-02', 'Starters', 'Yes'),
-	('Chocolate Brownie', 32.00, 'Yes', '2022-11-02', 'Dessert', 'Yes');
+insert into menu_item values(1,'Sandwich',99.00,'Yes','2017-03-15','Main Course','Yes','https://media.gettyimages.com/photos/ham-swiss-and-arugula-sandwich-picture-id846067582?s=2048x2048'),
+	(2,'Burger',129.00,'Yes','2017-12-27','Main Course','No','https://media.gettyimages.com/photos/lamb-greek-burger-picture-id637790866?s=2048x2048'),
+	(3,'Pizza', 149.00, 'Yes', '2018-08-21', 'Main Course', 'No','https://media.gettyimages.com/photos/whats-on-your-pizza-picture-id839988946?s=2048x2048'),
+	(4,'French Fries', 57.00, 'No', '2017-07-02', 'Starters', 'Yes','https://media.gettyimages.com/photos/basket-of-french-fries-picture-id614420426?s=2048x2048'),
+	(5,'Chocolate Brownie', 32.00, 'Yes', '2022-11-02', 'Dessert', 'Yes','https://media.gettyimages.com/photos/homemade-chocolate-brownies-shot-from-above-picture-id1130692246?s=2048x2048');
 
 
 SELECT mi.me_name,mi.me_price,mi.me_active,mi.me_date_of_launch,mi.me_category,mi.me_free_delivery FROM menu_item as mi;
